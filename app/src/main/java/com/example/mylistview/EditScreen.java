f0db36c71f0da44650357414e12970ce31af7cf0
Ronny <ronny761@hityash.org>
2021-11-01 15:18:55 +0200

    setonclick to button and activate selected radio button from radio group

--- app/src/main/java/com/example/mylistview/EditScreen.java
+++ app/src/main/java/com/example/mylistview/EditScreen.java
@@ -5,16 +5,18 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class EditScreen extends AppCompatActivity {
     Switch swtchdark;
     ConstraintLayout edit_layout;
     RadioGroup rg_type;
-    RadioButton rb_mind, rb_elec, rb_water;
+    RadioButton radioButton;
     Button btn_submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +26,22 @@ public class EditScreen extends AppCompatActivity {
         getSupportActionBar().hide(); //<< this
 
         rg_type = findViewById(R.id.rg_type);
-        rb_elec = findViewById(R.id.rb_electricity);
-        rb_mind = findViewById(R.id.rb_mind);
-        rb_water = findViewById(R.id.rb_water);
+
 
         btn_submit = findViewById(R.id.btn_submit);
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get selected radio button from radioGroup
+                int selectedId = rg_type.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioButton = findViewById(selectedId);
+
+                Toast.makeText(EditScreen.this,
+                        radioButton.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
         edit_layout = findViewById(R.id.edit_layout);
         swtchdark = findViewById(R.id.swtch_dark);
         Boolean switchState = swtchdark.isChecked();
