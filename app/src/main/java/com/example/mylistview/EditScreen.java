f050faec6584040f97b57f08ce96f33244c489eb
Ronny <ronny761@hityash.org>
2021-11-01 15:11:39 +0200

    referncing to radio buttons elements

--- app/src/main/java/com/example/mylistview/EditScreen.java
+++ app/src/main/java/com/example/mylistview/EditScreen.java
@@ -5,6 +5,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
@@ -14,13 +15,20 @@ public class EditScreen extends AppCompatActivity {
     ConstraintLayout edit_layout;
     RadioGroup rg_type;
     RadioButton rb_mind, rb_elec, rb_water;
+    Button btn_submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_screen);
 
         getSupportActionBar().hide(); //<< this
+
         rg_type = findViewById(R.id.rg_type);
+        rb_elec = findViewById(R.id.rb_electricity);
+        rb_mind = findViewById(R.id.rb_mind);
+        rb_water = findViewById(R.id.rb_water);
+
+        btn_submit = findViewById(R.id.btn_submit);
         edit_layout = findViewById(R.id.edit_layout);
         swtchdark = findViewById(R.id.swtch_dark);
         Boolean switchState = swtchdark.isChecked();
