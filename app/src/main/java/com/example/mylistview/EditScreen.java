315178426e9be48c2f868094ad7ef29e7fc1d1fe
Ronny <ronny761@hityash.org>
2021-11-01 14:55:12 +0200

    adding radoi button and switch to edit screen

--- app/src/main/java/com/example/mylistview/EditScreen.java
+++ app/src/main/java/com/example/mylistview/EditScreen.java
@@ -1,17 +1,34 @@
 package com.example.mylistview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class EditScreen extends AppCompatActivity {
-
+    Switch swtchdark;
+    ConstraintLayout edit_layout;
+    RadioGroup rg_type;
+    RadioButton rb_mind, rb_elec, rb_water;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_screen);
 
         getSupportActionBar().hide(); //<< this
+        rg_type = findViewById(R.id.rg_type);
+        edit_layout = findViewById(R.id.edit_layout);
+        swtchdark = findViewById(R.id.swtch_dark);
+        Boolean switchState = swtchdark.isChecked();
+        if(switchState)
+            edit_layout.setBackgroundColor(Color.BLACK);
+        else
+            edit_layout.setBackgroundColor(Color.WHITE);
+
 
     }
 }
