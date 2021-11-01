37ead8985c01be12f1b6eecc8aa22ba0e5e62921
Ronny <ronny761@hityash.org>
2021-11-01 14:34:52 +0200

    remove toolbar from editscreen

--- app/src/main/java/com/example/mylistview/EditScreen.java
+++ app/src/main/java/com/example/mylistview/EditScreen.java
@@ -10,5 +10,8 @@ public class EditScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_screen);
+
+        getSupportActionBar().hide(); //<< this
+
     }
 }
