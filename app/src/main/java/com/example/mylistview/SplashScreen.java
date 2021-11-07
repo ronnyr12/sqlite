8d9e6da999cd4bb6f560f9321cb939ca766eab79
rafaezagha <rafael.albagli.zagha@gmail.com>
2021-11-07 13:43:29 +0200

    Merge

--- app/src/main/java/com/example/mylistview/SplashScreen.java
+++ app/src/main/java/com/example/mylistview/SplashScreen.java
@@ -23,8 +23,8 @@ public class SplashScreen extends AppCompatActivity {
         db = openOrCreateDatabase(Utils.DATABASE_NAME,
                 MODE_PRIVATE, null);
 
-        createDatabase();
-        addALLToDb();
+        Utils.createTables(db);
+        Utils.addALLToDb(db);
 
         btnEnter = findViewById(R.id.enter);
         btnEnter.setOnClickListener(new View.OnClickListener() {
@@ -34,55 +34,6 @@ public class SplashScreen extends AppCompatActivity {
                         MainActivity.class));
             }
         });
-    }
-
-    /**
-     * custom method
-     * one time database creation
-     */
-    public void createDatabase(){
-
-        db.execSQL("create table if not exists tbl_pokemon(name text, power integer, type text)");
-        /* db.execSQL("create table if not exists "+Utils.TABLE_NAME_POKEMON+
-                " ("+Utils.TABLE_POKEMON_COL_NAME+" text, "+Utils.TABLE_POKEMON_COL_POWER+" integer, "+Utils.TABLE_POKEMON_COL_TYPE+" text)");
-        */
-    }
-
-    /**
-     * custom method
-     * adds values to database
-     * creates in background objects of pokemons
-     */
-    public void addALLToDb(){
-        ArrayList<Pokemon> pokemons = new ArrayList<>();
-
-        Pokemon pk0 = new Pokemon("Charmander", 1400, "Fire");
-        Pokemon pk1 = new Pokemon("Pikachu", 2350, "Electric");
-        Pokemon pk2 = new Pokemon("Charizard", 2700, "Fire");
-        Pokemon pk3 = new Pokemon("Eevee", 1800, "Normal");
-        Pokemon pk4 = new Pokemon("Snorlax", 2100, "Normal");
-        Pokemon pk5 = new Pokemon("Squirtle", 1600, "Water");
-        Pokemon pk6 = new Pokemon("Bulbasaur", 800, "Grass");
-        Pokemon pk7 = new Pokemon("Lucario", 1700, "Wind");
-        Pokemon pk8 = new Pokemon("Jigglypuff", 500, "Fairy");
-        Pokemon pk9 = new Pokemon("Ditto", 400, "Fairy");
-
-        pokemons.add(pk0);
-        pokemons.add(pk1);
-        pokemons.add(pk2);
-        pokemons.add(pk3);
-        pokemons.add(pk4);
-        pokemons.add(pk5);
-        pokemons.add(pk6);
-        pokemons.add(pk7);
-        pokemons.add(pk8);
-        pokemons.add(pk9);
-
-        for(Pokemon i : pokemons){
-            db.execSQL("insert into tbl_pokemon values('" + i.getName() + "', " + i.getPower() + ", '" + i.getType() + "')");
-        }
-
-
     }
 
 }
--- app/src/main/java/com/example/mylistview/Utils.java
+++ app/src/main/java/com/example/mylistview/Utils.java
@@ -1,12 +1,56 @@
 package com.example.mylistview;
 
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
 public class Utils {
+    final static String INTENT_KEY_POKEMON_NAME = "pokemon_name";
+
+
     final static String DATABASE_NAME = "db_pokemon_app";
+
     final static String TABLE_NAME_POKEMON = "tbl_pokemon";
     final static String TABLE_POKEMON_COL_NAME = "name";
     final static String TABLE_POKEMON_COL_POWER = "power";
     final static String TABLE_POKEMON_COL_TYPE = "type";
 
-    final static String INTENT_KEY_POKEMON_NAME = "pokemon_name";
+    public static void createTables(SQLiteDatabase db){
 
+        //db.execSQL("create table if not exists tbl_pokemon(name text, power integer, type text)");
+
+        db.execSQL("create table if not exists "+Utils.TABLE_NAME_POKEMON+
+                " ("+Utils.TABLE_POKEMON_COL_NAME+" text, "+Utils.TABLE_POKEMON_COL_POWER+" integer, "+Utils.TABLE_POKEMON_COL_TYPE+" text)");
+    }
+
+    public static void addALLToDb(SQLiteDatabase db){
+
+        ArrayList<Pokemon> pokemons = new ArrayList<>();
+
+        Pokemon pk0 = new Pokemon("Charmander", 1400, "Fire");
+        Pokemon pk1 = new Pokemon("Pikachu", 2350, "Electric");
+        Pokemon pk2 = new Pokemon("Charizard", 2700, "Fire");
+        Pokemon pk3 = new Pokemon("Eevee", 1800, "Normal");
+        Pokemon pk4 = new Pokemon("Snorlax", 2100, "Normal");
+        Pokemon pk5 = new Pokemon("Squirtle", 1600, "Water");
+        Pokemon pk6 = new Pokemon("Bulbasaur", 800, "Grass");
+        Pokemon pk7 = new Pokemon("Lucario", 1700, "Wind");
+        Pokemon pk8 = new Pokemon("Jigglypuff", 500, "Fairy");
+        Pokemon pk9 = new Pokemon("Ditto", 400, "Fairy");
+
+        pokemons.add(pk0);
+        pokemons.add(pk1);
+        pokemons.add(pk2);
+        pokemons.add(pk3);
+        pokemons.add(pk4);
+        pokemons.add(pk5);
+        pokemons.add(pk6);
+        pokemons.add(pk7);
+        pokemons.add(pk8);
+        pokemons.add(pk9);
+
+        for (Pokemon p: pokemons) {
+            db.execSQL("insert into tbl_pokemon values('"+p.getName()+"',"+p.getPower()+",'"+p.getType()+"')");
+        }
+    }
 }
